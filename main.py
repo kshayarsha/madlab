d89bd512dbc222067e159dbfbafe45d6fe981833
kshayarsha <121969433+kshayarsha@users.noreply.github.com>
2023-03-15 21:10:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-from turtle import Screen
-from snake import Snake
-import time
+from pandas import *
+from turtle import *
 
+
+class Score:
+    def __init__(self):
+        self.score = 0
+
+
+t1 = Turtle()
 screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
-
-snake = Snake()
-food = Food()
-
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
-
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-
-    snake.move()
-
+screen.screensize(1000,1000)
+screen.title('guess all the states')
+image = "states.gif"
+screen.addshape(image)
+t1.shape(image)
+guesses=[]
+data = read_csv('50_states.csv')
+states = data.state.to_list()
+sc = Score()
+t2 = Turtle()
+t2.hideturtle()
+t2.penup()
+on = True
+while on:
+    ans = screen.textinput(title=f'{sc.score}/50 correct', prompt='Enter next guess')
+    ans = ans.capitalize()
+    if ans == 'Exit':
+        for stated in states:
+            b = data[data.state == stated]
+            t2.goto(int(b.x), int(b.y))
+            t2.write(stated)
+        break
+    if ans in states:
+        sc.score += 1
+        a = data[data.state == ans]
+        xc = int(a.x)
+        yc = int(a.y)
+        t2.goto(xc, yc)
+        t2.write(ans)
+        if sc.score == 50:
+            on = False
 
 screen.exitonclick()
